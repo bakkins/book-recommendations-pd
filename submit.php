a56f3f72763849896229d0d8785205231d220067
Your Name <llaurisb07@gmail.com>
2025-09-24 11:46:37 +0300

    php 6.uzd neiet bet nevaru atrast kāpēc

--- submit.php
+++ submit.php
@@ -2,15 +2,31 @@
 $servername = "localhost";
 $username = "book_review_user_24092025";
 $password = "password";
+$dbname = "book_review_24092025";
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO book_review (full_name, book_title, review_text, rating) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("sssi", $name, $title, $review, $rating);
+
+$name = $_POST["formName"] + $_POST["formSurname"];
+$title = $_POST["formBookName"];
+$review = $_POST["formDesc"];
+$rating = $_POST["formLevel"];
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+$conn->close();
+
 ?>
 
 <a href="index.html">Atpakaļ uz formu</a>
