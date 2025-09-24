4db16917e72e9c97bfc828a14bc9bcd3a8da2125
Your Name <llaurisb07@gmail.com>
2025-09-24 11:11:50 +0300

    php 4.uzd izpildīts

--- submit.php
+++ submit.php
@@ -1,4 +1,16 @@
 <?php
-echo "<h1> Forma nosūtīta veiksmīgi.</h1>";
+$servername = "localhost";
+$username = "book_review_user_24092025";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
+
 <a href="index.html">Atpakaļ uz formu</a>
